1a59860d9b19bdd5b38d5d5d41b2f01aeeca21d8
Geonwoo Yoon <glove2275@gmail.com>
2026-04-17 14:31:00 +0900

    init

--- bash_src/create_env.sh
+++ bash_src/create_env.sh
@@ -1,4 +1,4 @@
-pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cpu
+pip install torch==2.6.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 pip install torch_geometric==2.6.1
-pip install pyg_lib torch_scatter torch_sparse -f https://data.pyg.org/whl/torch-2.6.0+cpu.html
+pip install pyg-lib torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.6.0+cu124.html
 pip install -r ../requirements.txt
